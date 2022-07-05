b6ebc1326ead4b4136167c3a7a998167900e0d07
Alina Mlynorik <alina.sudnick.@gmail.com>
2022-07-05 18:13:13 +0300

    demo-app-1: home page controller

---
+++ src/main/java/com/demoapp/HomeController.java
@@ -0,0 +1,12 @@
+package com.demoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String startMenu(){
+        return "menu";
+    }
+}
