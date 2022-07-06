2795ce1ef127c52b6b988f2392c5721eb60d70e9
Alina Mlynorik <alina.sudnick.@gmail.com>
2022-07-06 10:57:38 +0300

    demo-app-2.1: restructiolized

--- src/main/java/com/demoapp/controllers/HelloWorldController.java
+++ src/main/java/com/demoapp/controllers/HelloWorldController.java
@@ -5,8 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class HelloWorldController {
     @RequestMapping("/")
@@ -16,21 +14,21 @@ public class HelloWorldController {
     @RequestMapping("/show-form")
     public String showForm(Model model){
         model.addAttribute("formProcessing", "form-processing");
-        return "hello-world-form";
+        return "hello-world/hello-world-form";
     }
     @RequestMapping("/make-uppercase")
     public String showFormUppercase(Model model){
         model.addAttribute("formProcessing", "form-processing-uppercase");
-        return "hello-world-form";
+        return "hello-world/hello-world-form";
     }
     @RequestMapping("/form-processing")
     public String formProcessing(@RequestParam("studentName") String name, Model model){
         model.addAttribute("name", name);
-        return "hello-world-page";
+        return "hello-world/hello-world-page";
     }
     @RequestMapping("/form-processing-uppercase")
     public String formProcessingUppercase(@RequestParam("studentName") String name, Model model){
         model.addAttribute("name", name.toUpperCase());
-        return "hello-world-page";
+        return "hello-world/hello-world-page";
     }
 }
--- src/main/java/com/demoapp/controllers/StudentController.java
+++ src/main/java/com/demoapp/controllers/StudentController.java
@@ -3,13 +3,10 @@ package com.demoapp.controllers;
 import com.demoapp.models.Student;
 import com.demoapp.models.StudentOptionsData;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Map;
 
 @Controller
 @RequestMapping("/student")
@@ -26,12 +23,12 @@ public class StudentController {
         model.addAttribute("countryOptions", optionsData.getCountryOptions());
         model.addAttribute("programingLanguage", optionsData.getProgramingLanguage());
         model.addAttribute("operationSystem", optionsData.getOperatingSystem());
-        return "student-form";
+        return "student/student-form";
     }
 
     @RequestMapping("/form-processing")
     public String formProcessing(@ModelAttribute("student") Student student) {
         System.out.println(student.getFirstName() + " " + student.getLastName());
-        return "student-confirmation-page";
+        return "student/student-confirmation-page";
     }
 }
